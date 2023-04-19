88e7405a38b2aec760ea78d73faf7711c02cb8cb
Ankit2022u <ankit2022u@gmail.com>
2023-04-19 13:04:05 +0530

    Logs page created

--- admin/view_logs.php
+++ admin/view_logs.php
@@ -79,7 +79,8 @@ require "../api/dbcon.php";
             </div>
             <hr>
             <div class="profile">
-                <img src="../uploads/<?= $_SESSION['user-data']['user_type']; ?>/<?= $_SESSION['user-data']['profile_photo_path']; ?>" alt="Profile Pic" width="32" height="32" class="rounded-circle me-2">
+                <img src="../uploads/<?= $_SESSION['user-data']['user_type']; ?>/<?= $_SESSION['user-data']['profile_photo_path']; ?>"
+                    alt="Profile Pic" width="32" height="32" class="rounded-circle me-2">
                 <strong>
                     <?= $_SESSION['user-data']['officer_name']; ?>
                 </strong>
@@ -88,7 +89,80 @@ require "../api/dbcon.php";
         </div>
 
         <div class="main-content col-md-9 col-sm-7">
+            <table class="table table-bordered table-stripped">
+                <thead>
+                    <tr>
+                        <th>Serial Number</th>
+                        <th>Table Name</th>
+                        <th>Table ID</th>
+                        <th>Operation</th>
+                        <th>Description</th>
+                        <th>Log By</th>
+                        <th>Log Date</th>
+                        <th>Log Time</th>
+                        <th>When?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query1 = "SELECT * FROM logs ORDER BY created_at";
+                    $query_run1 = mysqli_query($con, $query1);
 
+                    if (mysqli_num_rows($query_run1) > 0) {
+                        foreach ($query_run1 as $log) {
+                            ?>
+                            <tr>
+                                <td>
+                                    <?= $log['lid']; ?>
+                                </td>
+                                <td>
+                                    <?= ucfirst($log['table_name']); ?>
+                                </td>
+                                <td>
+                                    <?= $log['table_id']; ?>
+                                </td>
+                                <td class="text-<?php
+                                    if($log['operation'] == "insert"){
+                                        echo "success";
+                                    }else if($log['operation'] == "update"){
+                                        echo "primary";
+                                    }
+                                    else{
+                                        echo "danger";
+                                    }
+                                ?>">
+                                    <?= ucfirst($log['operation']); ?>
+                                </td>
+                                <td>
+                                    <?= $log['log_desc']; ?>
+                                </td>
+                                <td>
+                                    <?php 
+                                    $user = $log['created_by'];
+                                    $query2 = "SELECT * FROM users WHERE user_id = '$user'";
+                                    $query_run2 = mysqli_query($con, $query2);
+                                    $officer = mysqli_fetch_array($query_run2)['officer_name']; ?>
+                                    <?= $officer; ?>
+                                </td>
+                                <td>
+                                    <?= (new DateTime($log['created_at']))->format('Y-m-d'); ?>
+                                </td>
+                                <td>
+                                    <?= (new DateTime($log['created_at']))->format('H:i:s'); ?>
+                                </td>
+                                <td>
+                                    Skip For Now
+                                </td>
+
+                            </tr>
+                            <?php
+                        }
+                    } else {
+                        echo "No Records Found";
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
 </main>
 
